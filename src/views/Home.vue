fc9d9555cc6d9cd2f9471aed06cd91645ed3fa48
warley junior <warleytho369@gmail.com>
2023-08-31 01:51:00 -0300

    aula-projeto

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+       <Banner />
+    <div class="main-container">
+        <h1>Monte seu burger</h1>
+        <BurgerForm/>
+    </div>
+</template>
+
+<script>
+import Banner from '../components/Banner.vue'
+import BurgerForm from '../components/BurgerForm.vue'
+
+
+
+export default {
+ name:"Home",
+ components:{
+    Banner,
+    BurgerForm
+
+}
+}
+</script>
+
+<style>
+
+</style>
